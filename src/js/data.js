08ca6b2c091dc59030a3551b12f1a7608fc4aa8f
CorentinCrz <ccroiz@live.fr>
2018-06-06 01:25:22 +0200

    archi over

--- src/js/data.js
+++ src/js/data.js
@@ -1,16 +1,16 @@
 var storyMain = [
   {
-    animationContent: {
-      img: './src/img/background.jpg',
-      css: 'class'
+    animation: {
+      img: false,
+      css: 'storyBackAnim'
     },
-    textContent: [
+    text: [
       {
         text: 'Bonjour machin, tu vas bien?',
         who: 'Tom'
       },
       {
-        text: 'Bonjour machin, tu vas bien?',
+        text: 'Bonjou vas bien?',
         who: 'Tom'
       }
     ],
@@ -22,6 +22,60 @@ var storyMain = [
       },
       {
         content: 'choix 2',
+        target: [0, 2]
+      }
+    ]
+  },
+  {
+    animation: {
+      img: './src/img/shutter.jpg',
+      css: 'storyShutter'
+    },
+    text: [
+      {
+        text: 'Bonjour machin2, tu vas bien?',
+        who: 'Vincent'
+      },
+      {
+        text: 'Bonjou2 vas bien?',
+        who: 'Tom'
+      }
+    ],
+    question: 'Que fai2re?',
+    answers: [
+      {
+        content: 'choix 4',
+        target: [0, 1]
+      },
+      {
+        content: 'choix 5',
+        target: [0, 2]
+      }
+    ]
+  },
+  {
+    animation: {
+      img: './src/img/background.jpg',
+      css: false
+    },
+    text: [
+      {
+        text: 'Bonjour machin2, tu vas bien?',
+        who: 'Vincent'
+      },
+      {
+        text: 'Bonjou2 vas bien?',
+        who: 'Tom'
+      }
+    ],
+    question: 'Que fai2re?',
+    answers: [
+      {
+        content: 'choix 4',
+        target: [0, 1]
+      },
+      {
+        content: 'choix 5',
         target: [0, 2]
       }
     ]
